9b65b9b6045299aab4c7f48501a2fdb4dbf6a396
Jongil Yoon <jiysait@gmail.com>
2021-06-11 08:19:43 -0600

    Redirection in Home

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,50 @@
  * Author: Jongil Yoon
  */
 
-import Link from "next/link"
-import { Container } from "semantic-ui-react"
-import TopNav from "../components/TopNav"
+import axios from "axios"
+// import Link from "next/link"
+import { useRouter } from "next/router"
+import { useCallback, useEffect } from "react"
+// import { Container } from "semantic-ui-react"
+// import TopNav from "../components/TopNav"
 
 
+/**
+ * Next.js recommend to handle redirection in backend 
+ * following code needs to be fixed
+ * @returns 
+ */
 export default function Home() {
+
+  // const [isLogin, setLogin] = useState(false)
+  const router = useRouter()
+
+  const checkLogIn = useCallback((e) => {
+    const url = '/api/verify'
+    axios.get(
+      url,
+    )
+      .then(res => {
+        if (res.status === 200) {
+          router.push('/sequences')
+          // setLogin(res.data.available)
+        } else {
+          router.push('/login')
+          // setLogin(false)
+        }
+      }).catch(err => {
+          router.push('/login')
+        // setLogin(false)
+      })
+  }, [])
+
+  useEffect(() => {
+    checkLogIn()
+  }, [])
 
   return (
     <>
-      <TopNav />
+      {/* <TopNav />
       <Container>
         <br />
         <br />
@@ -26,7 +60,7 @@ export default function Home() {
             <li><Link href={'/examples/uniqueness'}>Guaranteed Uniqueness of Files (working on)</Link></li>
           </ul>
         </div>
-      </Container>
+      </Container> */}
     </>
   )
 }
